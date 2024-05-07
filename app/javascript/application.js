77da6e5523f5cd7c36a97f21d3847ef025234acf
HERUESTA <zgbh07@gmail.com>
2024-05-07 21:34:00 +0900

    実装

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,4 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import * as bootstrap from "bootstrap/dist/js/bootstrap"
-import './lib/bootstrap_confirm'
 //= require data-confirm-modal
